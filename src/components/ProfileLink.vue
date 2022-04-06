80124187d3b57be3a22bf357e09b1bd7cd76b284
Juan David Zea <vikzea@gmail.com>
2022-04-06 12:26:41 -0500

    WIP

--- src/components/ProfileLink.vue
+++ src/components/ProfileLink.vue
@@ -5,22 +5,24 @@ import { SelectorIcon } from '@heroicons/vue/solid';
 <template>
     <router-link
         :to="{ name: 'profile' }"
-        class="hover:bg-black/10 flex gap-4 cursor-pointer rounded-lg px-4 py-2 w-80"
+        class="hover:bg-black/10 flex cursor-pointer rounded-lg px-4 py-2 w-80 justify-between"
     >
-        <div>
-            <img
-                width="50"
-                height="50"
-                src="https://ui-avatars.com/api/?name=John+Doe&rounded=true&background=089cd8&bold=true&size=50"
-                alt="avatar image"
-            />
-        </div>
-        <div class="flex flex-col justify-center">
-            <h1 class="font-medium">Juan David</h1>
-            <span class="font-medium text-gray-500">juan.zea@gmail.com</span>
+        <div class="flex gap-4">
+            <div>
+                <img
+                    width="50"
+                    height="50"
+                    src="https://ui-avatars.com/api/?name=John+Doe&rounded=true&background=089cd8&bold=true&size=50"
+                    alt="avatar image"
+                />
+            </div>
+            <div class="flex flex-col justify-center">
+                <h1 class="font-medium">Juan David</h1>
+                <span class="font-medium text-gray-500">juan.zea@gmail.com</span>
+            </div>
         </div>
         <div class="flex items-center">
-            <SelectorIcon class="h-5" />
+            <SelectorIcon class="h-5 text-gray-400" />
         </div>
     </router-link>
 </template>
